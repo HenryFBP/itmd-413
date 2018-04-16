5fb82d932b128b71040dc55b9ccede6a77fd7eb7
Henry Post <HenryFBP@gmail.com>
2018-04-16 17:50:01 -0500

    refactor many methods

--- labs/lab8/__init__.py
+++ labs/lab8/__init__.py
@@ -1,6 +1,7 @@
 import os
 
-from labs.lab7.tkContacts import loadListFromFile, Contacts
+from labs.lab8.tkContacts import contact_list_from_path, ContactsGUI, Contact
+from labs.lab8.contacts import contactlistdata
 
 contacts_file = 'contacts.txt'
 
@@ -8,8 +9,10 @@ if __name__ == '__main__':
 
     if os.path.exists(contacts_file):
         print("'" + contacts_file + "' exists! Loading values from it...")
-        contactlist = loadListFromFile(contacts_file)
-
-    contacts = Contacts(contactlist, contacts_file)
+        contactlist = contact_list_from_path(contacts_file)
+    else:
+        print(f"'{contacts_file}' doesn't exist. Using default data.")
+        contactlist = contactlistdata
+    contacts = ContactsGUI(contactlist, contacts_file)
 
     contacts.root.mainloop()
--- labs/lab8/tkContacts.py
+++ labs/lab8/tkContacts.py
@@ -2,35 +2,68 @@ from tkinter import *
 
 
 class Contact:
-    def __init__(self, name, phone):
-        self.name = name
-        self.phone = phone
+    @staticmethod
+    def from_line(line: str):
+        lines = line.split(',')
+
+        name = ','.join(lines[:-1])  # all but last
+        phone = lines[-1]  # last
+
+        contact = Contact(name, phone)
+        print(contact)
+        return contact
+
+    def __init__(self, name: str, phone: str):
+        name = name.strip()
+        phone = phone.strip()
+
+        self.name: str = name
+        self.phone: str = phone
+
+    def __str__(self):
+        return f"{self.name}, {self.phone}"
+
+    def __lt__(self, other):
+        return self.name > other.name
+
+    def __gt__(self, other):
+        return self.name > other.name
+
+    def __iter__(self):
+        yield self.name
+        yield self.phone
 
 
-class Contacts:
+class ContactsGUI:
     def selection(self):
         print("At %s of %d" % (self.select.curselection(), len(self.contacts)))
         return int(self.select.curselection()[0])
 
-    def getName(self):
+    def get_name(self):
         return self.nameVar.get()
 
-    def getPhone(self):
+    def get_phone(self):
         return self.phoneVar.get()
 
-    def addContact(self):
-        self.contacts.append([self.getName(), self.phoneVar.get()])
-        self.setList()
+    def get_contact_from_fields(self):
+        return Contact(self.get_name(), self.get_phone())
 
-    def updateContact(self):
-        self.contacts[self.selection()] = [self.nameVar.get(), self.phoneVar.get()]
-        self.setList()
+    def get_selected(self):
+        return self.contacts[self.selection()]
 
-    def deleteContact(self):
+    def add_contact(self):
+        self.contacts.append(self.get_contact_from_fields())
+        self.set_list()
+
+    def update_selected_contact(self):
+        self.contacts[self.selection()] = self.get_contact_from_fields()
+        self.set_list()
+
+    def delete_selected_contact(self):
         del self.contacts[self.selection()]
-        self.setList()
+        self.set_list()
 
-    def loadContact(self):
+    def load_selected_contact(self):
         name, phone = self.contacts[self.selection()]
         self.nameVar.set(name)
         self.phoneVar.set(phone)
@@ -58,11 +91,11 @@ class Contacts:
 
         self.framebuttons = Frame(self.root)  # add a row of buttons
         self.framebuttons.pack()
-        self.btn_add = Button(self.framebuttons, text=" Add  ", command=self.addContact)
-        self.btn_update = Button(self.framebuttons, text="Update", command=self.updateContact)
-        self.btn_delete = Button(self.framebuttons, text="Delete", command=self.deleteContact)
-        self.btn_load = Button(self.framebuttons, text=" Load ", command=self.loadContact)
-        self.btn_save = Button(self.framebuttons, text=" Save ", command=self.setList)
+        self.btn_add = Button(self.framebuttons, text=" Add  ", command=self.add_contact)
+        self.btn_update = Button(self.framebuttons, text="Update", command=self.update_selected_contact)
+        self.btn_delete = Button(self.framebuttons, text="Delete", command=self.delete_selected_contact)
+        self.btn_load = Button(self.framebuttons, text=" Load ", command=self.load_selected_contact)
+        self.btn_save = Button(self.framebuttons, text=" Save ", command=self.set_list)
 
         self.btn_add.pack(side=LEFT)
         self.btn_update.pack(side=LEFT)
@@ -78,39 +111,31 @@ class Contacts:
         self.scroll.pack(side=RIGHT, fill=Y)
         self.select.pack(side=LEFT, fill=BOTH)
 
-        self.setList()
+        self.set_list()
 
-    def setList(self):
+    def set_list(self):
+        """Resets displayed list from self.contacts."""
         self.contacts.sort()
         self.select.delete(0, END)
-        for name, phone in self.contacts:
-            self.select.insert(END, name)
 
+        for contact in self.contacts:
+            self.select.insert(END, contact.name)
+
+        self.save_to_file()
+
+    def save_to_file(self):
         with open(self.path, 'w') as f:
-
             print("Writing list to file at '" + self.path + "'.")
 
             for contact in self.contacts:
-                line = ','.join(contact)
+                line = str(contact)
                 f.write(line + '\n')
 
 
-def contactFromLine(line: str):
-    lines = line.split(',')
-
-    fname = lines[0]
-    lname = lines[1]
-    pn = lines[2]
-
-    ret = [(fname + ', ' + lname), pn]
-    print(ret)
-    return ret
-
-
-def loadListFromFile(path: str):
+def contact_list_from_path(path: str):
     ret = []
     with open(path, 'r') as f:
         for line in f:
             line = line.replace('\r', '').replace('\n', '')
-            ret.append(contactFromLine(line))
+            ret.append(Contact.from_line(line))
     return ret
