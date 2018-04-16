aa8cdc5503bd19f8fec33ce97c25524ac18577c0
HenryFBP <HenryFBP@gmail.com>
2018-04-16 15:49:20 -0500

    add lab 8 skel

---
+++ labs/lab8/__init__.py
@@ -0,0 +1,10 @@
+
+if __name__ == '__main__':
+
+    if os.path.exists(contacts_file):
+        print("'" + contacts_file + "' exists! Loading values from it...")
+        contactlist = loadListFromFile(contacts_file)
+
+    contacts = Contacts(contactlist, contacts_file)
+
+    contacts.root.mainloop()
